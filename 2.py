863520f24cfbb618069f9bdff167c66bdfb04376
ynion-mabeamae <mabeamaeynion@gmail.com>
2024-10-12 01:22:11 +0800

    Upload file

---
+++ 2.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "The number is even.")
+elif num % 2 != 0:
+    print(num, "The number is odd.")
+else:
+    print(num, "The number is zero.")
