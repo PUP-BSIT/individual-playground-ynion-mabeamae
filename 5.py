d7a4b816ea103da58a2c03058a1d569e54689ce0
ynion-mabeamae <mabeamaeynion@gmail.com>
2024-10-12 01:23:40 +0800

    Upload file

---
+++ 5.py
@@ -0,0 +1,8 @@
+character = input("Enter a character: ")
+
+character = character.lower()
+
+if character in 'aeiou':
+    print("The character is a vowel.")
+else:
+    print("The character is a consonant.b")
